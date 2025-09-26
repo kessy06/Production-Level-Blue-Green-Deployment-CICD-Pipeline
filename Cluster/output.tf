caee9d73c115c7797e81fba406466fad0ef1e613
kingsley <kessy06@yahoo.com>
2025-09-26 23:08:09 +0100

    first commit

--- Cluster/output.tf
+++ Cluster/output.tf
@@ -1,16 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.blue_green.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.blue_green.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.blue_green_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.blue_green_subnet[*].id
 }
-
